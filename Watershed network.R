3bf339d3031e6ea1d8892f6e5efff3a52d683a59
Kyle Wilson <kyle.logan.wilson@gmail.com>
2018-10-29 17:49:00 -0700

    change the graph plot

--- Watershed network.R
+++ Watershed network.R
@@ -57,6 +57,8 @@ dist2 <- rep(diameter/2,(NlocalPops*Nbranches))
 dist3 <- rep(diameter/3,(NlocalPops*Nbranches*Nforks))
 E(g1)$weight <- c(dist1,dist2,dist3)
 
+node.size<-setNames(c(2,rep(0.8,NlocalPops),rep(0.8,NlocalPops*Nbranches),rep(0.8,NlocalPops*Nbranches*Nforks)),V(g1)$names)
+
 colfunc(1)
 
 V(g1)$color[c(1,as.vector(t(sapply(LETTERS[1:NlocalPops],function(x){grep(x,V(g1)$name)}))))] <- c("grey50",as.vector(t(sapply(LETTERS[1:NlocalPops],function(x){rep(colfunc(NlocalPops)[which(x==LETTERS[1:NlocalPops])],length(grep(x,V(g1)$name)))}))))
@@ -64,7 +66,7 @@ V(g1)$color[c(1,as.vector(t(sapply(LETTERS[1:NlocalPops],function(x){grep(x,V(g1
 layout(1)
 par(mar=c(5,4,1,1))
 tiff("watershed.tiff",compression="lzw",units="in",height=8,width=8,res=800)
-plot(g1,col="dodgerblue",layout=layout.reingold.tilford(g1,circular=T))
+plot(g1,col="dodgerblue",layout=layout.reingold.tilford(g1,circular=T),vertex.size=node.size*15)
 dev.off()
 
 distance_table(g1)
