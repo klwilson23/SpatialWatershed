3e567233f6f54137275a584d64428f05a8d980c8
Kyle Wilson <kyle.logan.wilson@gmail.com>
2019-03-07 12:28:10 -0800

    adding various ways to calculate stock-recruit parameters from metapopulation dynamics

--- Metapop sim model.R
+++ Metapop sim model.R
@@ -15,8 +15,8 @@ m <- 0.15 # distance decay function: could set at some proportion of max distanc
 alpha <- 20
 metaK <- 25000
 beta <- -log(alpha)/metaK
-cv <- 0.1
-alpha_heterogeneity <- FALSE
+cv <- 0.5
+alpha_heterogeneity <- TRUE
 beta_heterogeneity <- TRUE
 
 alpha_p <- rep(alpha,Npatches)
@@ -40,10 +40,11 @@ if(beta_heterogeneity)
 k_p <- -log(alpha_p)/beta_p
 sum(k_p)
 
-stock <- 0:(round(max(k_p))+1)
-rec_mean <- sapply(1:Npatches,function(x){alpha_p[x]*stock*exp(beta_p[x]*stock)})
+stock <- 1:(2*round(max(k_p))+1)
+rec_mean <- sapply(1:Npatches,function(x){pmax(0.01,rnorm(length(stock),
+                                                mean=alpha_p[x]*stock*exp(beta_p[x]*stock),
+                                                sd=(alpha_p[x]*stock*exp(beta_p[x]*stock))*cv))})
 
-dim(rec_mean)
 
 metaRec <- rowSums(rec_mean)
 metaStock <- stock*Npatches
@@ -52,12 +53,24 @@ metaStock <- stock*Npatches
 metaRicker <- lm(log(metaRec/metaStock)~metaStock)
 
 SR_df <- data.frame("stock"=metaStock,"recruits"=metaRec)
+fit_nls <- nls(recruits~alpha*stock*exp(-(log(alpha)/K)*stock),data=SR_df,start=list("alpha"=alpha,"K"=metaK),algorithm="port")
 
-fit_nls <- nls(recruits~alpha*stock*exp(-(log(alpha)/K)*stock),data=SR_df,start=list("alpha"=alpha,"K"=metaK))
-
+ricker_SR <- function(l_alpha,l_K,l_sd){
+  sd <- exp(l_sd)
+  K <- exp(l_K)
+  alpha <- exp(l_alpha)
+  rec_mean <- alpha*SR_df$stock*exp(-(log(alpha)/K)*SR_df$stock)
+  nll <- -(sum(dnorm(SR_df$recruits,mean=rec_mean,sd=sd,log=T),na.rm=T))
+  return("nll"=nll)
+}
+theta <- list("l_alpha"=log(20),"l_K"=log(25000),"l_sd"=log(1000))
+mle_fit <- mle(ricker_SR,start=theta,method="SANN",nobs=nrow(SR_df))
+exp(mle_fit@coef)
 meta_alpha <- exp(coef(metaRicker)[1])
 meta_beta <- coef(metaRicker)[2]
--log(meta_alpha)/meta_beta
+
+print(c(meta_alpha,meta_beta,-log(meta_alpha)/meta_beta))
+summary(fit_nls)
 
 plot(metaStock,metaRec,type="l",lwd=2,col="dodgerblue")
 invisible(sapply(1:Npatches,function(x){
