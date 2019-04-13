8b818a3e78806d518f3e843884335f2f25865142
Kyle Wilson <kyle.logan.wilson@gmail.com>
2019-04-12 18:11:59 -0700

    comment on dispersal function

--- Dispersal function.R
+++ Dispersal function.R
@@ -5,7 +5,7 @@ dispersal <- function(maxDispersal,distDecay,dist_matrix,recruitment) # recruitm
   maxDispersal <- maxDispersal # dispersal rate
   distDecay <- distDecay # distance penalty for migration
   Npops <- ncol(dist_matrix)
-  migrationNumer <- apply(dist_matrix,2,function(x){exp(-distDecay*x)}) # straying between sites, Anderson et al. 2015 EcoApps
+  migrationNumer <- apply(dist_matrix,2,function(x){exp(-distDecay*x)}) # dispersal between sites, Anderson et al. 2015 EcoApps
   diag(migrationNumer) <- 0 # fish cannot migrate to their own population
   migration <- maxDispersal*migrationNumer/colSums(migrationNumer)
   # rows correspond to emigrants away from patch
